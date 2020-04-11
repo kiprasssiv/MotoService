aa24c404473588933feecb5072c492840e877105
KiprasSivickas <sivickaskipras@gmail.com>
2020-04-11 13:29:32 +0300

    Started adding new service

--- build/resources/main/db/migration/V002__insert_motorcycle_data.sql
+++ build/resources/main/db/migration/V002__insert_motorcycle_data.sql
@@ -1,3 +1,3 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-INSERT INTO motorcycles values (uuid_generate_v4(),'Suzuki','Gs450',1984,true);
-INSERT INTO motorcycles values (uuid_generate_v4(),'Suzuki','Gs450',1984,true);
+INSERT INTO motorcycles values (uuid_generate_v4(),'Suzuki','Gs450',1884,true);
+INSERT INTO motorcycles values (uuid_generate_v4(),'Suzuki','G50',1984,true);
